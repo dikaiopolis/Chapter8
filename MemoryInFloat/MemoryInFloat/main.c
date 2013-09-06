141624767c49a9e01daf6890a3aa9b65316a63ec
Daniel Baldwin <daniel.j.baldwin@hotmail.com>
2013-09-06 14:49:06 -0500

    exercise files

---
+++ MemoryInFloat/MemoryInFloat/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  MemoryInFloat
+//
+//  Created by Daniel Baldwin on 8/12/13.
+//  Copyright (c) 2013 Daniel Baldwin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    printf("A float is %zu bytes\n", sizeof(float));
+    return 0;
+}
+
